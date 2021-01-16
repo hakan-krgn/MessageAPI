1762a2676ff99f946275842bbd08665427d920cc
Hakan <hakankargin_2003@hotmail.com>
2021-01-17 00:35:12 +0300

    edit

--- src/main/java/com/hakan/messageplugin/title/titlenms/nms/Title_v1_16_R2.java
+++ src/main/java/com/hakan/messageplugin/title/titlenms/nms/Title_v1_16_R2.java
@@ -13,8 +13,8 @@ public class Title_v1_16_R2 implements TitleNMS.TitlePacket {
     @Override
     public void send(Player player, Title title) {
 
-        IChatBaseComponent titleString = IChatBaseComponent.ChatSerializer.a("\"" + title.getTitle() + "\":\"" + title.getTitle() + "\"");
-        IChatBaseComponent subtitleString = IChatBaseComponent.ChatSerializer.a("\"" + title.getSubtitle() + "\":\"" + title.getSubtitle() + "\"");
+        IChatBaseComponent titleString = IChatBaseComponent.ChatSerializer.a("{\"text\":\"" + title.getTitle() + "\"}");
+        IChatBaseComponent subtitleString = IChatBaseComponent.ChatSerializer.a("{\"text\":\"" + title.getSubtitle() + "\"}");
 
         PacketPlayOutTitle packetPlayOutTitle = new PacketPlayOutTitle(PacketPlayOutTitle.EnumTitleAction.TITLE, titleString, title.getFadeIn(), title.getStay(), title.getFadeOut());
         PacketPlayOutTitle packetPlayOutSubTitle = new PacketPlayOutTitle(PacketPlayOutTitle.EnumTitleAction.SUBTITLE, subtitleString, title.getFadeIn(), title.getStay(), title.getFadeOut());
