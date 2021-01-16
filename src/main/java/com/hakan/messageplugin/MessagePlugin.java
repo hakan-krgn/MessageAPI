29102754cbd0851d88fef648c567f3d472999c3e
Hakan <hakankargin_2003@hotmail.com>
2021-01-16 15:34:41 +0300

    edit

--- src/main/java/com/hakan/messageplugin/MessagePlugin.java
+++ src/main/java/com/hakan/messageplugin/MessagePlugin.java
@@ -8,7 +8,6 @@ import org.bukkit.Bukkit;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.scheduler.BukkitRunnable;
 
 public class MessagePlugin extends JavaPlugin {
 
@@ -29,14 +28,5 @@ public class MessagePlugin extends JavaPlugin {
     @Override
     public void onEnable() {
         MessageAPI.setup(this);
-
-        new BukkitRunnable() {
-            @Override
-            public void run() {
-                MessageAPI.getTitleAPI().getTitleManager().setTitle("sa").setSubtitle("as").setStay(20).create().sendAll();
-                MessageAPI.getActionBarAPI().getActionBarManager().setText("sa").create().sendAll();
-                MessageAPI.getBarAPI().getBarManager().create().addPlayer(Bukkit.getPlayer("blueybighats"));
-            }
-        }.runTaskLater(this, 20 * 5);
     }
 }
