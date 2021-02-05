8d39a23e9a1cafd0b9eafbcc233fcbb21ba57c81
Hakan <hakankargin_2003@hotmail.com>
2021-02-05 18:44:28 +0300

    edit

--- src/main/java/com/hakan/messageplugin/title/titlenms/nms/Title_v1_12_R1.java
+++ src/main/java/com/hakan/messageplugin/title/titlenms/nms/Title_v1_12_R1.java
@@ -2,6 +2,7 @@ package com.hakan.messageplugin.title.titlenms.nms;
 
 import com.hakan.messageplugin.title.Title;
 import com.hakan.messageplugin.title.titlenms.TitleNMS;
+import net.md_5.bungee.api.chat.BaseComponent;
 import net.minecraft.server.v1_12_R1.IChatBaseComponent;
 import net.minecraft.server.v1_12_R1.PacketPlayOutTitle;
 import net.minecraft.server.v1_12_R1.PlayerConnection;
@@ -16,10 +17,14 @@ public class Title_v1_12_R1 implements TitleNMS.TitlePacket {
         IChatBaseComponent titleString = IChatBaseComponent.ChatSerializer.a("{\"text\":\"" + title.getTitle() + "\"}");
         IChatBaseComponent subtitleString = IChatBaseComponent.ChatSerializer.a("{\"text\":\"" + title.getSubtitle() + "\"}");
 
+        PacketPlayOutTitle packetPlayOutTitleTime = new PacketPlayOutTitle(PacketPlayOutTitle.EnumTitleAction.TIMES, (BaseComponent[]) null, title.getFadeIn(), title.getStay(), title.getFadeOut());
+        PacketPlayOutTitle packetPlayOutSubTitleTime = new PacketPlayOutTitle(PacketPlayOutTitle.EnumTitleAction.TIMES, (BaseComponent[]) null, title.getFadeIn(), title.getStay(), title.getFadeOut());
         PacketPlayOutTitle packetPlayOutTitle = new PacketPlayOutTitle(PacketPlayOutTitle.EnumTitleAction.TITLE, titleString, title.getFadeIn(), title.getStay(), title.getFadeOut());
         PacketPlayOutTitle packetPlayOutSubTitle = new PacketPlayOutTitle(PacketPlayOutTitle.EnumTitleAction.SUBTITLE, subtitleString, title.getFadeIn(), title.getStay(), title.getFadeOut());
 
         PlayerConnection playerConnection = ((CraftPlayer) player).getHandle().playerConnection;
+        playerConnection.sendPacket(packetPlayOutTitleTime);
+        playerConnection.sendPacket(packetPlayOutSubTitleTime);
         playerConnection.sendPacket(packetPlayOutTitle);
         playerConnection.sendPacket(packetPlayOutSubTitle);
     }
--- src/main/java/com/hakan/messageplugin/title/titlenms/nms/Title_v1_16_R2.java
+++ src/main/java/com/hakan/messageplugin/title/titlenms/nms/Title_v1_16_R2.java
@@ -16,10 +16,14 @@ public class Title_v1_16_R2 implements TitleNMS.TitlePacket {
         IChatBaseComponent titleString = IChatBaseComponent.ChatSerializer.a("{\"text\":\"" + title.getTitle() + "\"}");
         IChatBaseComponent subtitleString = IChatBaseComponent.ChatSerializer.a("{\"text\":\"" + title.getSubtitle() + "\"}");
 
+        PacketPlayOutTitle packetPlayOutTitleTime = new PacketPlayOutTitle(PacketPlayOutTitle.EnumTitleAction.TIMES, null, title.getFadeIn(), title.getStay(), title.getFadeOut());
+        PacketPlayOutTitle packetPlayOutSubTitleTime = new PacketPlayOutTitle(PacketPlayOutTitle.EnumTitleAction.TIMES, null, title.getFadeIn(), title.getStay(), title.getFadeOut());
         PacketPlayOutTitle packetPlayOutTitle = new PacketPlayOutTitle(PacketPlayOutTitle.EnumTitleAction.TITLE, titleString, title.getFadeIn(), title.getStay(), title.getFadeOut());
         PacketPlayOutTitle packetPlayOutSubTitle = new PacketPlayOutTitle(PacketPlayOutTitle.EnumTitleAction.SUBTITLE, subtitleString, title.getFadeIn(), title.getStay(), title.getFadeOut());
 
         PlayerConnection playerConnection = ((CraftPlayer) player).getHandle().playerConnection;
+        playerConnection.sendPacket(packetPlayOutTitleTime);
+        playerConnection.sendPacket(packetPlayOutSubTitleTime);
         playerConnection.sendPacket(packetPlayOutTitle);
         playerConnection.sendPacket(packetPlayOutSubTitle);
     }
