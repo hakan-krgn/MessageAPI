55ded2aa7cc19a6e89d2033b15cba74b038fca96
Hakan <hakankargin_2003@hotmail.com>
2021-03-08 13:34:43 +0300

    edit

--- src/main/java/com/hakan/messageplugin/title/titlenms/nms/Title_v1_8_R3.java
+++ src/main/java/com/hakan/messageplugin/title/titlenms/nms/Title_v1_8_R3.java
@@ -2,7 +2,6 @@ package com.hakan.messageplugin.title.titlenms.nms;
 
 import com.hakan.messageplugin.title.Title;
 import com.hakan.messageplugin.title.titlenms.TitleNMS;
-import net.md_5.bungee.api.chat.BaseComponent;
 import net.minecraft.server.v1_8_R3.IChatBaseComponent;
 import net.minecraft.server.v1_8_R3.PacketPlayOutTitle;
 import net.minecraft.server.v1_8_R3.PlayerConnection;
@@ -17,8 +16,8 @@ public class Title_v1_8_R3 implements TitleNMS.TitlePacket {
         IChatBaseComponent titleString = IChatBaseComponent.ChatSerializer.a("{\"text\":\"" + title.getTitle() + "\"}");
         IChatBaseComponent subtitleString = IChatBaseComponent.ChatSerializer.a("{\"text\":\"" + title.getSubtitle() + "\"}");
 
-        PacketPlayOutTitle packetPlayOutTitleTime = new PacketPlayOutTitle(PacketPlayOutTitle.EnumTitleAction.TIMES, (BaseComponent[]) null, title.getFadeIn(), title.getStay(), title.getFadeOut());
-        PacketPlayOutTitle packetPlayOutSubTitleTime = new PacketPlayOutTitle(PacketPlayOutTitle.EnumTitleAction.TIMES, (BaseComponent[]) null, title.getFadeIn(), title.getStay(), title.getFadeOut());
+        PacketPlayOutTitle packetPlayOutTitleTime = new PacketPlayOutTitle(PacketPlayOutTitle.EnumTitleAction.TIMES, null, title.getFadeIn(), title.getStay(), title.getFadeOut());
+        PacketPlayOutTitle packetPlayOutSubTitleTime = new PacketPlayOutTitle(PacketPlayOutTitle.EnumTitleAction.TIMES, null, title.getFadeIn(), title.getStay(), title.getFadeOut());
         PacketPlayOutTitle packetPlayOutTitle = new PacketPlayOutTitle(PacketPlayOutTitle.EnumTitleAction.TITLE, titleString, title.getFadeIn(), title.getStay(), title.getFadeOut());
         PacketPlayOutTitle packetPlayOutSubTitle = new PacketPlayOutTitle(PacketPlayOutTitle.EnumTitleAction.SUBTITLE, subtitleString, title.getFadeIn(), title.getStay(), title.getFadeOut());
 
